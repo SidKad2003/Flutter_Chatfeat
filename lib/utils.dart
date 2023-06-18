f1493b2de994afb252cd5d2f22724123bec325a5
Mankirat Singh <msingh2_be22@thapar.edu>
2023-06-18 15:07:59 +0530

    Top bar widget created
    Signed-off-by: Mankirat Singh <msingh2_be22@thapar.edu>

--- lib/utils.dart
+++ lib/utils.dart
@@ -80,3 +80,63 @@ TextStyle SafeGoogleFont(
     );
   }
 }
+
+AppBar topBar(String title, BuildContext context, String image) {
+  return AppBar(
+    leading: Builder(
+      builder: (BuildContext context) {
+        return IconButton(
+          icon: const Icon(Icons.list),
+          onPressed: () {},
+          iconSize: 40,
+          color: Colors.black,
+        );
+      },
+    ),
+    title: Text(title,
+        style: SafeGoogleFont(
+          "Roboto",
+          textStyle: const TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 0, 0, 0),
+          ),
+        )),
+    centerTitle: true,
+    actions: [
+      Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: const LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromARGB(255, 71, 224, 215),
+              Color.fromARGB(255, 84, 102, 121)
+            ],
+          ),
+        ),
+        margin: EdgeInsets.only(right: 10),
+        child: CircleAvatar(
+          radius: MediaQuery.of(context).size.height * 0.032,
+          backgroundColor: Colors.transparent,
+          child: CircleAvatar(
+            radius: MediaQuery.of(context).size.height * 0.029,
+            backgroundImage: AssetImage(image),
+          ),
+        ),
+      ),
+    ],
+    flexibleSpace: Container(
+      child: Stack(
+        children: <Widget>[
+          Image.asset(
+            'assests/img/Up image rectangle.png',
+            fit: BoxFit.cover,
+            width: MediaQuery.of(context).size.width,
+          ),
+        ],
+      ),
+    ),
+  );
+}
